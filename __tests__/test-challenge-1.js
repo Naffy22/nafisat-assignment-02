ab1fe1fca9f331c5104d1515f5baabb4db7ce37e
Nafisat Idris <108720394+Naffy22@users.noreply.github.com>
2022-07-15 21:02:21 +0000

    check challenge 1

--- __tests__/test-challenge-1.js
+++ __tests__/test-challenge-1.js
@@ -3,8 +3,8 @@ const assignment = require('../src/assignment.js');
 const { sumOfNumbers } = assignment;
 const maybe = sumOfNumbers === undefined ? test.skip : test;
 
-maybe('Challenge - 1 : my sumNumbers function can sum an array of numbers', () => {
-    const numbers = [
+maybe('Challenge - 1 : my sumNumbers function can sum an array of numbers', (a,b) =>a+b); {
+    const numbers = [1,2,5,6,7,8];
         68,
         -68,
         27,
